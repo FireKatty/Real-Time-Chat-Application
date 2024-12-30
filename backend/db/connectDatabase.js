d2b80836dbbf9c98744755e6daad7d6fa41957ba
firekatty <abhishekkatiyar1947@gmail.com>
2024-12-31 02:00:53 +0530

    database url change

--- backend/db/connectDatabase.js
+++ backend/db/connectDatabase.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose")
 
+const MONGO_URI = "mongodb+srv://abhishekkatiyar0203:abhishekkatiyar0203@cluster0.d7f03.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 const coonectToDatabase = async()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/Chat_Application')
+        await mongoose.connect(MONGO_URI)
         console.log("database connected")
     } catch (error) {
         console.log("Error")
