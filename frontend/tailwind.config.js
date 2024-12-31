cef7ae655315e43ecbb945be4f7576c5f8acb043
firekatty <abhishekkatiyar1947@gmail.com>
2024-12-31 13:26:38 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [
-    // eslint-disable-next-line no-undef
-    require('daisyui')
+    daisyui, // Using the imported DaisyUI plugin
   ],
-}
+};
